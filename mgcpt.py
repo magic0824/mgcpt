595ebd86540196ec9db6c22ea9e221ae50d4e4d7
magic0824 <yuta.ogiso0824@gmail.com>
2022-08-04 12:27:04 +0900

    Add files

---
+++ mgcpt.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import platform
+import os
+import sys
+
+pretty_name=""
+
+def get_command():
+	try:
+		return sys.argv[1]
+	except:
+		return ""
+
+def get_package():
+	try:
+		return sys.argv[2]
+	except:
+		return ""
+
+def help_command():
+	print('mgcpt.py - The package tool for MGCLinux')
+	print('mgcpt.py help|install|install-wapp name\n')
+	print('help - Show this help')
+	print('install - Install application(beta)')
+	print('install-wapp - Install web application')
+	return
+
+def y_or_n():
+	print('(y/N)')
+	yn=input()
+	yn='{}'.format(yn.lower())
+	if yn == 'y' or yn == 'ye' or yn == 'yes':
+		return True
+	elif yn == 'n' or yn == 'no':
+		return False
+	else:
+		return False
+	return
+
+def install(name):
+	print('This feature is under development.')
+	return
+
+def install_wapp(name):
+	if name == '':
+		print('No packages spectified.')
+		return
+	print('Are you sure to install {}? '.format(name),end='')
+	if y_or_n() == False:
+		print('Install is stoped.')
+		return
+	
+	print('Successfuly installed {}"'.format(name))
+	return
+
+if __name__ == '__main__': 
+	command = get_command()
+	if command == "":
+		print('No command.\n')
+		help_command()
+	elif command == "help":
+		help_command()
+	elif command == "install":
+		package=get_package()
+		install(package)
+	elif command == "install-wapp":
+		package=get_package()
+		install_wapp(package)
